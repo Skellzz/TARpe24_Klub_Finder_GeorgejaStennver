cbd58ee56eb1d7c17f9124f67827f777e805af70
Opilane TTHK <opilane@tthk.ee>
2026-05-05 12:48:40 +0300

    s

---
+++ Klub_Finder/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
+namespace Klub_Finder.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+    public interface IId { Get; Set }
+
+}
