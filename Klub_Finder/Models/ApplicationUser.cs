8f8dcdaf97fddd1d1ca5a50edd6058e041bd0298
Opilane TTHK <163452729+Skellzz@users.noreply.github.com>
2026-05-08 15:29:54 +0300

    Deleted one uhh thing

--- Klub_Finder/Models/ApplicationUser.cs
+++ Klub_Finder/Models/ApplicationUser.cs
@@ -4,7 +4,6 @@ namespace Klub_Finder.Models
     public class ApplicationUser : IdentityUser
     {
         public string DisplayName { get; set; }
-        public string ConfirmPassword { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
