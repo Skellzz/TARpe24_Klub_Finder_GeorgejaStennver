114f026ca783f38fd416cb4a58647c722ddea9ab
Opilane TTHK <163452729+Skellzz@users.noreply.github.com>
2026-05-08 15:34:06 +0300

    awd

--- Klub_Finder/Data/ApplicationDbContext.cs
+++ Klub_Finder/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Dynamic;
 namespace Klub_Finder.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -12,6 +12,5 @@ namespace Klub_Finder.Data
         }
 
         public DbSet<Klubi> Klubid { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
