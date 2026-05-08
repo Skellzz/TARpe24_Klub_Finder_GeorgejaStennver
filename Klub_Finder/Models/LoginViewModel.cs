596a2919d3d59b28fb857937864321224fcd8372
Opilane TTHK <163452729+Skellzz@users.noreply.github.com>
2026-05-08 10:58:19 +0300

    Tegin LoginViewModelsi

--- Klub_Finder/Models/LoginViewModel.cs
+++ Klub_Finder/Models/LoginViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Klub_Finder.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Klub_Finder.Models
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Rember me ?")]
+        public bool RemeberMe { get; set; }
     }
 }
