29aa9b771659334137e232968a9712aeed0a2a4b
Skellzz <george.teemus@gmail.com>
2026-05-12 10:56:07 +0300

    s

--- Klub_Finder/Controllers/AccountController.cs
+++ Klub_Finder/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Klub_Finder.Controllers
 {
@@ -78,13 +79,13 @@ namespace Klub_Finder.Controllers
             }
             return View(model);
         }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Login", "Account");
-        }
-
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Logout()
+        //{
+           // await _signInManager.SignOutAsync();
+            //return RedirectToAction("Login", "Account");
+       // }
+       
     }
 }
