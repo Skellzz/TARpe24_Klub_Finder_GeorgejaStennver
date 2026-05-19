4678beb9f1623dc0849bdaf02206a6bed9edd9df
Opilane TTHK <opilane@tthk.ee>
2026-05-19 12:54:14 +0300

    ma lisasin Toit.cshtml i koodi ja tegin paar mudelit juurde

--- Klub_Finder/Models/Toit.cs
+++ Klub_Finder/Models/Toit.cs
@@ -2,5 +2,8 @@
 {
     public class Toit
     {
+        public int Id { get; set; }
+        public string FoodName { get; set;}
+        public DateTime RegisteerimisKuupäev { get; set; } = DateTime.Now;
     }
 }
