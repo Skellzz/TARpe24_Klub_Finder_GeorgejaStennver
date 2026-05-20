6f166862b0b647a073a051288e74b0201c0fa092
Skellzz <george.teemus@gmail.com>
2026-05-20 10:31:20 +0300

    Sain FoodAndDrinks Indexi tööle

---
+++ Klub_Finder/Controllers/FoodAndDrinksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Klub_Finder.Controllers
+{
+    public class FoodAndDrinksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- Klub_Finder/Controllers/HomeController.cs
+++ Klub_Finder/Controllers/HomeController.cs
@@ -28,6 +28,7 @@ namespace Klub_Finder.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
