08197988938474d06ef6e949762e86c95e775a66
Skellzz <george.teemus@gmail.com>
2026-05-24 14:46:38 +0300

    Lisasin Food.cs i Models folderisse ja lisasin selle sisse andmed mida andmin saab pärast lisada andmebaasi

---
+++ Klub_Finder/Models/Food.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Klub_Finder.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string FoodName { get; set;}
+        public string Description { get; set; }
+
+        [Column(TypeName = "Decimal(18,2)")]
+        public decimal Price { get; set; }
+        public string? ImagePath { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        public DateTime RegisteerimisKuupäev { get; set; } = DateTime.Now;
+
+    }
+}
