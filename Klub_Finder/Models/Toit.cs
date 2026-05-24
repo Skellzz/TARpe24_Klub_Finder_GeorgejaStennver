22de9e6dd5daf6a730898c6e8da589b1cd9181ed
Skellzz <george.teemus@gmail.com>
2026-05-24 14:47:06 +0300

    Nimetasin Toit.cs-i Food.cs-iks

--- Klub_Finder/Models/Toit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Klub_Finder.Models
-{
-    public class Toit
-    {
-        public int Id { get; set; }
-        public string FoodName { get; set;}
-        public DateTime RegisteerimisKuupäev { get; set; } = DateTime.Now;
-    }
-}
