9e207194d794551149e2de92acd79095763f9358
Stenver <stenver.lindmaa@gmail.com>
2026-05-25 20:10:11 +0300

    Jook

---
+++ Klub_Finder/Models/Jook.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Klub_Finder.Models
+{
+    public class Jook
+    {
+        public int Id { get; set; }
+        public string DrinkName { get; set; }
+        public string DrinkType { get; set; }
+        public int AlcoholPercentage { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        public DateTime RegistreerimsKuupäevn { get; set; } = DateTime.Now;
+    }
+}
