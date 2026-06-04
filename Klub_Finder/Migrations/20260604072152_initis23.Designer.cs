c9affcc0b643f25c0616e2a253792a5a39fc0f12
Stenver <stenver.lindmaa@gmail.com>
2026-06-04 11:17:10 +0300

    migrad

--- Klub_Finder/Migrations/20260604072152_initis23.Designer.cs
+++ Klub_Finder/Migrations/20260604072152_initis23.Designer.cs
@@ -4,6 +4,7 @@ using Klub_Finder.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Klub_Finder.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260604072152_initis23")]
+    partial class initis23
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
